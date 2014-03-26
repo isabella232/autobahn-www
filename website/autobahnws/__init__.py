b07d05e091a972bad5b6010e51f33cf950fd49da
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-03-26 10:48:53 +0100

    fixes

--- website/autobahnws/__init__.py
+++ website/autobahnws/__init__.py
@@ -49,17 +49,17 @@ def page_impressum():
 ## AutobahnPython
 ##
 
-@app.route('/python/')
-def page_python():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'overview'
-   return render_template('python/page_tpnd_python.html')
+# @app.route('/python/')
+# def page_python():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'overview'
+#    return render_template('python/page_tpnd_python.html')
 
-@app.route('/python/getstarted/')
-def page_python_getstarted():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'getstarted'
-   return render_template('python/page_tpnd_python_getstarted.html')
+# @app.route('/python/getstarted/')
+# def page_python_getstarted():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'getstarted'
+#    return render_template('python/page_tpnd_python_getstarted.html')
 
 # @app.route('/python/reference/')
 # def page_python_reference():
@@ -80,82 +80,82 @@ def page_python_getstarted():
 #    session['subsitetab'] = 'tutorials'
 #    return render_template('python/page_tpn_python_examples.html')
 
-@app.route('/python/examples/')
-def page_python_examples():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'tutorials'
-   return render_template('python/page_tpn_python_examples_external.html')
+# @app.route('/python/examples/')
+# def page_python_examples():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('python/page_tpn_python_examples_external.html')
 
-@app.route('/python/tutorials/')
-def page_python_tutorials_old():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'tutorials'
-   return render_template('python/page_tpn_python_tutorials.html')
+# @app.route('/python/tutorials/')
+# def page_python_tutorials_old():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('python/page_tpn_python_tutorials.html')
 
-@app.route('/python/tutorials/echo/')
-def page_python_tutorials_echo():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'tutorials'
-   return render_template('python/page_tpnd_python_tutorials_echo.html')
+# @app.route('/python/tutorials/echo/')
+# def page_python_tutorials_echo():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('python/page_tpnd_python_tutorials_echo.html')
 
-@app.route('/python/tutorials/streaming/')
-def page_python_tutorials_streaming():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'tutorials'
-   return render_template('python/page_tpnd_python_tutorials_streaming.html')
+# @app.route('/python/tutorials/streaming/')
+# def page_python_tutorials_streaming():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('python/page_tpnd_python_tutorials_streaming.html')
 
-@app.route('/python/tutorials/producerconsumer/')
-def page_python_tutorials_producerconsumer():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'tutorials'
-   return render_template('python/page_tpnd_python_tutorials_producerconsumer.html')
+# @app.route('/python/tutorials/producerconsumer/')
+# def page_python_tutorials_producerconsumer():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('python/page_tpnd_python_tutorials_producerconsumer.html')
 
-@app.route('/python/tutorials/rpc/')
-def page_python_tutorials_rpc():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'tutorials'
-   return render_template('python/page_tpnd_python_tutorials_rpc.html')
+# @app.route('/python/tutorials/rpc/')
+# def page_python_tutorials_rpc():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('python/page_tpnd_python_tutorials_rpc.html')
 
-@app.route('/python/tutorials/pubsub/')
-def page_python_tutorials_pubsub():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'tutorials'
-   return render_template('python/page_tpnd_python_tutorials_pubsub.html')
+# @app.route('/python/tutorials/pubsub/')
+# def page_python_tutorials_pubsub():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('python/page_tpnd_python_tutorials_pubsub.html')
 
-@app.route('/python/downloads/')
-def page_python_downloads():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'downloads'
-   return render_template('python/page_tpn_python_downloads.html')
+# @app.route('/python/downloads/')
+# def page_python_downloads():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'downloads'
+#    return render_template('python/page_tpn_python_downloads.html')
 
-@app.route('/python/getintouch/')
-def page_python_getintouch():
-   session['subsite'] = 'python'
-   session['subsitetab'] = 'getintouch'
-   return render_template('page_tpn_getintouch.html')
+# @app.route('/python/getintouch/')
+# def page_python_getintouch():
+#    session['subsite'] = 'python'
+#    session['subsitetab'] = 'getintouch'
+#    return render_template('page_tpn_getintouch.html')
 
 
 ################################################################################
 ## AutobahnAndroid
 ##
 
-@app.route('/android/')
-def page_android():
-   session['subsite'] = 'android'
-   session['subsitetab'] = 'overview'
-   return render_template('android/page_tpnd_android.html')
+# @app.route('/android/')
+# def page_android():
+#    session['subsite'] = 'android'
+#    session['subsitetab'] = 'overview'
+#    return render_template('android/page_tpnd_android.html')
 
-@app.route('/android/getstarted/')
-def page_android_getstarted():
-   session['subsite'] = 'android'
-   session['subsitetab'] = 'getstarted'
-   return render_template('android/page_tpnd_android_getstarted.html')
+# @app.route('/android/getstarted/')
+# def page_android_getstarted():
+#    session['subsite'] = 'android'
+#    session['subsitetab'] = 'getstarted'
+#    return render_template('android/page_tpnd_android_getstarted.html')
 
-@app.route('/android/tutorials/')
-def page_android_tutorials():
-   session['subsite'] = 'android'
-   session['subsitetab'] = 'tutorials'
-   return render_template('android/page_tpn_android_tutorials.html')
+# @app.route('/android/tutorials/')
+# def page_android_tutorials():
+#    session['subsite'] = 'android'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('android/page_tpn_android_tutorials.html')
 
 # @app.route('/android/reference/')
 # def page_android_reference():
@@ -163,146 +163,146 @@ def page_android_tutorials():
 #    session['subsitetab'] = 'reference'
 #    return render_template('android/page_tpn_android_reference.html')
 
-@app.route('/android/downloads/')
-def page_android_downloads():
-   session['subsite'] = 'android'
-   session['subsitetab'] = 'downloads'
-   return render_template('android/page_tpn_android_downloads.html')
+# @app.route('/android/downloads/')
+# def page_android_downloads():
+#    session['subsite'] = 'android'
+#    session['subsitetab'] = 'downloads'
+#    return render_template('android/page_tpn_android_downloads.html')
 
-@app.route('/android/getintouch/')
-def page_android_getintouch():
-   session['subsite'] = 'android'
-   session['subsitetab'] = 'getintouch'
-   return render_template('page_tpn_getintouch.html')
+# @app.route('/android/getintouch/')
+# def page_android_getintouch():
+#    session['subsite'] = 'android'
+#    session['subsitetab'] = 'getintouch'
+#    return render_template('page_tpn_getintouch.html')
 
 
 ################################################################################
 ## AutobahnJS
 ##
 
-@app.route('/js/')
-def page_js():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'overview'
-   return render_template('js/page_tpnd_js.html')
+# @app.route('/js/')
+# def page_js():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'overview'
+#    return render_template('js/page_tpnd_js.html')
 
-@app.route('/js/getstarted/')
-def page_js_getstarted():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'getstarted'
-   return render_template('js/page_tpnd_js_getstarted.html')
+# @app.route('/js/getstarted/')
+# def page_js_getstarted():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'getstarted'
+#    return render_template('js/page_tpnd_js_getstarted.html')
 
-@app.route('/js/tutorials/')
-def page_js_tutorials():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'tutorials'
-   return render_template('js/page_tpn_js_tutorials.html')
+# @app.route('/js/tutorials/')
+# def page_js_tutorials():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('js/page_tpn_js_tutorials.html')
 
-@app.route('/js/tutorials/pubsub/')
-def page_js_tutorials_pubsub():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'tutorials'
-   return render_template('js/page_tpnd_js_tutorials_pubsub.html')
+# @app.route('/js/tutorials/pubsub/')
+# def page_js_tutorials_pubsub():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('js/page_tpnd_js_tutorials_pubsub.html')
 
-@app.route('/js/tutorials/rpc/')
-def page_js_tutorials_rpc():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'tutorials'
-   return render_template('js/page_tpnd_js_tutorials_rpc.html')
+# @app.route('/js/tutorials/rpc/')
+# def page_js_tutorials_rpc():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'tutorials'
+#    return render_template('js/page_tpnd_js_tutorials_rpc.html')
 
-@app.route('/js/reference/')
-def page_js_reference():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'reference'
-   return render_template('js/page_tpsd_js_reference.html')
+# @app.route('/js/reference/')
+# def page_js_reference():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'reference'
+#    return render_template('js/page_tpsd_js_reference.html')
 
-@app.route('/js/downloads/')
-def page_js_downloads():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'downloads'
-   return render_template('js/page_tpn_js_downloads.html')
+# @app.route('/js/downloads/')
+# def page_js_downloads():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'downloads'
+#    return render_template('js/page_tpn_js_downloads.html')
 
-@app.route('/js/getintouch/')
-def page_js_getintouch():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'getintouch'
-   return render_template('page_tpn_getintouch.html')
+# @app.route('/js/getintouch/')
+# def page_js_getintouch():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'getintouch'
+#    return render_template('page_tpn_getintouch.html')
 
-@app.route('/js/wampv1/')
-def page_js_wampv1():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'wampv1'
-   return render_template('js/page_tpnd_js_wampv1.html')
+# @app.route('/js/wampv1/')
+# def page_js_wampv1():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'wampv1'
+#    return render_template('js/page_tpnd_js_wampv1.html')
 
-@app.route('/js/wampv1/getstarted/')
-def page_js_wampv1_getstarted():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'wampv1'
-   return render_template('js/page_tpnd_js_wampv1_getstarted.html')
+# @app.route('/js/wampv1/getstarted/')
+# def page_js_wampv1_getstarted():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'wampv1'
+#    return render_template('js/page_tpnd_js_wampv1_getstarted.html')
 
-@app.route('/js/wampv1/tutorials/')
-def page_js_wampv1_tutorials():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'wampv1'
-   return render_template('js/page_tpn_js_wampv1_tutorials.html')
+# @app.route('/js/wampv1/tutorials/')
+# def page_js_wampv1_tutorials():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'wampv1'
+#    return render_template('js/page_tpn_js_wampv1_tutorials.html')
 
-@app.route('/js/wampv1/tutorials/pubsub/')
-def page_js_wampv1_tutorials_pubsub():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'wampv1'
-   return render_template('js/page_tpnd_js_wampv1_tutorials_pubsub.html')
+# @app.route('/js/wampv1/tutorials/pubsub/')
+# def page_js_wampv1_tutorials_pubsub():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'wampv1'
+#    return render_template('js/page_tpnd_js_wampv1_tutorials_pubsub.html')
 
-@app.route('/js/wampv1/tutorials/rpc/')
-def page_js_wampv1_tutorials_rpc():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'wampv1'
-   return render_template('js/page_tpnd_js_wampv1_tutorials_rpc.html')
+# @app.route('/js/wampv1/tutorials/rpc/')
+# def page_js_wampv1_tutorials_rpc():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'wampv1'
+#    return render_template('js/page_tpnd_js_wampv1_tutorials_rpc.html')
 
-@app.route('/js/wampv1/reference/')
-def page_js_wampv1_reference():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'wampv1'
-   return render_template('js/page_tpsd_js_wampv1_reference.html')
+# @app.route('/js/wampv1/reference/')
+# def page_js_wampv1_reference():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'wampv1'
+#    return render_template('js/page_tpsd_js_wampv1_reference.html')
 
-@app.route('/js/wampv1/downloads/')
-def page_js_wampv1_downloads():
-   session['subsite'] = 'js'
-   session['subsitetab'] = 'wampv1'
-   return render_template('js/page_tpn_js_wampv1_downloads.html')
+# @app.route('/js/wampv1/downloads/')
+# def page_js_wampv1_downloads():
+#    session['subsite'] = 'js'
+#    session['subsitetab'] = 'wampv1'
+#    return render_template('js/page_tpn_js_wampv1_downloads.html')
 
 ################################################################################
 ## AutobahnTestsuite
 ##
 
-@app.route('/testsuite/')
-def page_testsuite():
-   session['subsite'] = 'testsuite'
-   session['subsitetab'] = 'overview'
-   return render_template('testsuite/page_tp_testsuite.html')
+# @app.route('/testsuite/')
+# def page_testsuite():
+#    session['subsite'] = 'testsuite'
+#    session['subsitetab'] = 'overview'
+#    return render_template('testsuite/page_tp_testsuite.html')
 
-@app.route('/testsuite/installation/')
-def page_testsuite_installation():
-   session['subsite'] = 'testsuite'
-   session['subsitetab'] = 'installation'
-   return render_template('testsuite/page_tp_testsuite_installation.html')
+# @app.route('/testsuite/installation/')
+# def page_testsuite_installation():
+#    session['subsite'] = 'testsuite'
+#    session['subsitetab'] = 'installation'
+#    return render_template('testsuite/page_tp_testsuite_installation.html')
 
-@app.route('/testsuite/manual/')
-def page_testsuite_manual():
-   session['subsite'] = 'testsuite'
-   session['subsitetab'] = 'manual'
-   return render_template('testsuite/page_tp_testsuite_manual.html')
+# @app.route('/testsuite/manual/')
+# def page_testsuite_manual():
+#    session['subsite'] = 'testsuite'
+#    session['subsitetab'] = 'manual'
+#    return render_template('testsuite/page_tp_testsuite_manual.html')
 
-@app.route('/testsuite/downloads/')
-def page_testsuite_downloads():
-   session['subsite'] = 'testsuite'
-   session['subsitetab'] = 'downloads'
-   return render_template('testsuite/page_tpn_testsuite_downloads.html')
+# @app.route('/testsuite/downloads/')
+# def page_testsuite_downloads():
+#    session['subsite'] = 'testsuite'
+#    session['subsitetab'] = 'downloads'
+#    return render_template('testsuite/page_tpn_testsuite_downloads.html')
 
-@app.route('/testsuite/getintouch/')
-def page_testsuite_getintouch():
-   session['subsite'] = 'testsuite'
-   session['subsitetab'] = 'getintouch'
-   return render_template('page_tpn_getintouch.html')
+# @app.route('/testsuite/getintouch/')
+# def page_testsuite_getintouch():
+#    session['subsite'] = 'testsuite'
+#    session['subsitetab'] = 'getintouch'
+#    return render_template('page_tpn_getintouch.html')
 
 
 if __name__ == "__main__":
